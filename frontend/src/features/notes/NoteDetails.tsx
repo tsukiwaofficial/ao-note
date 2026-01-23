20f1ffdce9fc0d3ff903c1f3190d25dc054a59f9
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-01-23 17:01:07 +0800

    add update note functionality

--- frontend/src/features/notes/NoteDetails.tsx
+++ frontend/src/features/notes/NoteDetails.tsx
@@ -1,10 +1,23 @@
-import type { Note } from "./note.types";
-import { FaTrash, FaPencil } from "react-icons/fa6";
-import { useNoteContext } from "./useNoteContext";
+import { useState } from "react";
 import { formatDistanceToNow } from "date-fns";
+import type { Note } from "./note.types";
+import { FaTrash, FaPencil, FaCheck, FaXmark } from "react-icons/fa6";
+import { useNoteContext } from "./useNoteContext";
 
-export default function NoteDetails({ _id, title, content, createdAt }: Note) {
+export default function NoteDetails({
+  _id,
+  title,
+  content,
+  createdAt,
+  updatedAt,
+}: Note) {
   const { dispatch } = useNoteContext();
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const [noteData, setNoteData] = useState<Note>({
+    title,
+    content,
+  });
+  const [error, setError] = useState<string>("");
 
   const deleteNote = async () => {
     const response = await fetch(
@@ -22,14 +35,75 @@ export default function NoteDetails({ _id, title, content, createdAt }: Note) {
     else throw new Error("Note ID is missing");
   };
 
+  const updateNote = async () => {
+    setIsUpdating(!isUpdating);
+
+    if (isUpdating) {
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND_URL + "/notes/" + _id,
+        {
+          method: "PUT",
+          body: JSON.stringify(noteData),
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+
+      const result = await response.json();
+
+      if (!response.ok) setError(result.message);
+
+      dispatch({
+        type: "UPDATE_NOTE",
+        payload: { _id, ...noteData, createdAt, updatedAt },
+      });
+    }
+  };
+
+  const handleUpdateChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setNoteData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="p-8 shadow bg-surface max-h-max rounded-lg hover:-translate-y-1 hover:shadow-lg transition-transform">
       <div className="w-full flex items-start justify-between gap-10">
         <div className="space-y-4">
-          <div className="">
-            <h5 className="line-clamp-1 capitalize text-primary">{title}</h5>
-            <p className="line-clamp-2">{content}</p>
+          <div className="flex flex-col w-full">
+            {isUpdating ? (
+              <input
+                type="text"
+                name="title"
+                id="update-title"
+                className="mb-2"
+                placeholder="Title"
+                value={noteData.title}
+                onChange={handleUpdateChange}
+              />
+            ) : (
+              <h5 className="line-clamp-1 capitalize text-primary mb-1">
+                {title}
+              </h5>
+            )}
+            {isUpdating ? (
+              <textarea
+                name="content"
+                id="update-content"
+                className=""
+                placeholder="Content"
+                value={noteData.content}
+                onChange={handleUpdateChange}
+              />
+            ) : (
+              <p className="line-clamp-2">{content}</p>
+            )}
           </div>
+          {error && (
+            <div className="rounded-lg text-error p-4 border-2 border-error bg-error/30 w-max">
+              {error}
+            </div>
+          )}
         </div>
         <div className="flex flex-col items-center justify-between">
           <button
@@ -38,17 +112,39 @@ export default function NoteDetails({ _id, title, content, createdAt }: Note) {
           >
             <FaTrash />
           </button>
-          <button className="rounded-full p-4 hover:bg-primary cursor-pointer hover:text-white transition-colors">
-            <FaPencil />
+          <button
+            onClick={updateNote}
+            className="rounded-full p-4 hover:bg-primary cursor-pointer hover:text-white transition-colors"
+          >
+            {isUpdating ? <FaCheck /> : <FaPencil />}
           </button>
+          {isUpdating && (
+            <button
+              onClick={() => setIsUpdating(false)}
+              className="rounded-full p-4 hover:bg-error cursor-pointer hover:text-white transition-colors"
+            >
+              <FaXmark />
+            </button>
+          )}
         </div>
       </div>
       {createdAt && (
-        <span className="text-sm text-slate-400">
+        <span className="text-sm text-slate-400 capitalize">
           {formatDistanceToNow(new Date(createdAt), {
             addSuffix: true,
             includeSeconds: true,
           })}
+        </span>
+      )}
+      {updatedAt && createdAt !== updatedAt && (
+        <span className="text-sm text-slate-400 capitalize">
+          {" "}
+          (Updated{" "}
+          {formatDistanceToNow(new Date(updatedAt), {
+            addSuffix: true,
+            includeSeconds: true,
+          })}
+          )
         </span>
       )}
     </div>
