c5aa8282da2b6f9492f7132ce70a52a74fd04e94
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-01-26 18:59:10 +0800

    update homepage notes and banner layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import type { Note } from "../features/notes/note.types";
 import NoteDetails from "../features/notes/NoteDetails";
 import NoteForm from "../features/notes/NoteForm";
 import { useNoteContext } from "../features/notes/useNoteContext";
+import Banner from "../components/Banner";
 
 export default function Home() {
   const { state: notes, dispatch } = useNoteContext();
@@ -21,27 +22,34 @@ export default function Home() {
   }, [dispatch]);
 
   return (
-    <div className="container mx-auto py-50 flex justify-between gap-30">
-      <div className="space-y-5 w-full">
-        <h3>Notes</h3>
-        <div className="h-max grid grid-cols-1 gap-y-4">
-          {notes.length > 0 ? (
-            notes.map(({ _id, title, content, createdAt, updatedAt }: Note) => (
-              <NoteDetails
-                key={_id}
-                _id={_id}
-                title={title}
-                content={content}
-                createdAt={createdAt}
-                updatedAt={updatedAt}
-              />
-            ))
-          ) : (
-            <div className="">No notes found</div>
-          )}
+    <div className="container mx-auto pt-50 pb-25 space-y-15">
+      <div className="flex justify-between gap-20">
+        <div className="space-y-5 w-full">
+          <h3>Notes</h3>
+          <div className="h-max grid grid-cols-1 gap-y-5">
+            {notes.length > 0 ? (
+              notes.map(
+                ({ _id, title, content, createdAt, updatedAt }: Note) => (
+                  <NoteDetails
+                    key={_id}
+                    _id={_id}
+                    title={title}
+                    content={content}
+                    createdAt={createdAt}
+                    updatedAt={updatedAt}
+                  />
+                ),
+              )
+            ) : (
+              <div className="">No notes found</div>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-10">
+          <NoteForm />
+          <Banner />
         </div>
       </div>
-      <NoteForm />
     </div>
   );
 }
