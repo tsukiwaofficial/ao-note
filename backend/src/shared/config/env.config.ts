9d4f2b31f64402233e79cde2059d5ab2911b67e5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-20 19:20:58 +0800

    add new env variables to env configuration

--- backend/src/shared/config/env.config.ts
+++ backend/src/shared/config/env.config.ts
@@ -4,3 +4,7 @@ export const MONGODB_URI: string = verifyEnv("MONGODB_URI");
 export const PORT: string = verifyEnv("PORT");
 export const BACKEND_URL: string = verifyEnv("BACKEND_URL");
 export const FRONTEND_URL: string = verifyEnv("FRONTEND_URL");
+export const ACCESS_SECRET: string = verifyEnv("ACCESS_SECRET");
+export const REFRESH_SECRET: string = verifyEnv("REFRESH_SECRET");
+export const MODE: string = verifyEnv("MODE");
+export const isProduction: boolean = MODE === "production";
