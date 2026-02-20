5b28c1c907de5b3c3ad0c43a6780fceb54a5374e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-20 19:28:22 +0800

    new styling to ui variants

--- frontend/src/shared/config/ui-variants/form-variants.config.ts
+++ frontend/src/shared/config/ui-variants/form-variants.config.ts
@@ -5,6 +5,7 @@ export const formVariants = cva("w-full", {
     variant: {
       default:
         "flex flex-col xl:flex-row items-end xl:items-start justify-between gap-10",
+      login: "px-15 py-20 bg-surface/80 backdrop-blur-lg",
     },
   },
   defaultVariants: {
