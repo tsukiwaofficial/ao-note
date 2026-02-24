5be3a940c5dd15bdf8623baeb0f8666cb036117f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-02-24 09:05:56 +0800

    adjust styling to Section and Footer layouts

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const date = new Date().getFullYear();
 
   return (
-    <footer className="pb-20 text-center text-sm text-slate-500">
+    <footer className="w-full h-max pb-20 text-center text-sm text-slate-500">
       <p>Copyright {date} © tsukiwa. All rights reserved.</p>
     </footer>
   );
--- frontend/src/layouts/Section.tsx
+++ frontend/src/layouts/Section.tsx
@@ -4,7 +4,7 @@ type SectionProps = HTMLAttributes<HTMLElement>;
 
 export default function Section({ children, className }: SectionProps) {
   return (
-    <section className={`relative ao-note-container pt-50 pb-25 ${className}`}>
+    <section className={`relative ao-note-container pt-30 pb-25 ${className}`}>
       {children}
     </section>
   );
