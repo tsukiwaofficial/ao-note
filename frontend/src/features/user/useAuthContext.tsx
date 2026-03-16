6a1113656d33990f705c0302d431e0cd36320fb0
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2026-03-16 17:05:57 +0800

    log update to context hook

--- frontend/src/features/user/useAuthContext.tsx
+++ frontend/src/features/user/useAuthContext.tsx
@@ -5,7 +5,7 @@ export const useAuthContext = () => {
   const context = useContext(AuthContext);
 
   if (!context)
-    throw new Error("useAuthContext must be used within a UserProvider");
+    throw new Error("useAuthContext must be used within a AuthProvider");
 
   return context;
 };
