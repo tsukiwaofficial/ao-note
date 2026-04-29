1a3c4321234bf8fdd5d25eba100f7b5a25c57e5d
Jam <moonbamijamofficial15@gmail.com>
2026-04-29 15:00:26 +0800

    update user logout logic

--- frontend/src/features/user/useUserLogout.tsx
+++ frontend/src/features/user/useUserLogout.tsx
@@ -1,25 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { aoNoteFetch } from "../../shared/utils/http/ao-note-fetch.util";
 import { useNoteContext } from "../notes/useNoteContext";
-import { useUserContext } from "./useUserContext";
-import type { UserDetails } from "./user.types";
-import { useAuthActions } from "./useUserAuthStore";
-import { useCookies } from "react-cookie";
+import { useUserAuthActions } from "./useUserAuthStore";
 
 export const useUserLogout = () => {
   const navigate = useNavigate();
-  const [cookies] = useCookies(["isLoggedIn"]);
   const { dispatch: noteDispatch } = useNoteContext();
-  const { dispatch: userDetailsDispatch } = useUserContext();
-  const { useInitializeGuestAuth } = useAuthActions();
+  const { initializeGuestAuth } = useUserAuthActions();
 
   const logout = async () => {
     await aoNoteFetch("/api/users/logout", { method: "POST" });
     navigate("/login");
-    useInitializeGuestAuth(cookies.isLoggedIn);
+    initializeGuestAuth();
 
     noteDispatch({ type: "GET_NOTES", payload: [] });
-    userDetailsDispatch({ type: "GET_USER", payload: {} as UserDetails });
   };
 
   return {
