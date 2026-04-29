15a7cbb03a99aa3117be4c1eb49ca719613b8121
Jam <moonbamijamofficial15@gmail.com>
2026-04-29 14:55:58 +0800

    update types for user feat

--- frontend/src/features/user/user.types.ts
+++ frontend/src/features/user/user.types.ts
@@ -1,42 +1,67 @@
-import type { Dispatch } from "react";
 import type { JwtPayload } from "jwt-decode";
 import type { MongoDbDefaults } from "../../shared/types/mongodb.types";
 
-export interface User extends MongoDbDefaults {
+export interface User {
   username: string;
   password: string;
 }
 
 export type ConfirmUser = User & { confirmPassword: string };
 
+export interface UserAuth {
+  _id: string;
+  role: "user" | "guest" | "";
+  token: string;
+}
+
+export interface UserAuthResponse extends UserAuth {
+  message: string;
+  error?: string;
+}
+
+interface UserAuthActions {
+  actions: {
+    login: (user: User) => Promise<{
+      response: Response;
+      result: UserAuthResponse;
+    }>;
+    signup: (user: User) => Promise<{
+      response: Response;
+      result: UserAuthResponse;
+    }>;
+    initializeUserAuth: (cookie: string) => Promise<void>;
+    refreshUserAuth: (token: string, cookie: string) => void;
+    initializeGuestAuth: () => void;
+  };
+}
+
+export interface UserAuthStore extends UserAuth, UserAuthActions {}
+
 export interface UserDetails extends MongoDbDefaults {
   avatar: string;
   displayName: string;
 }
 
-export interface UserAuth {
-  _id: string;
-  role: string;
-  token: string;
+export type UserDetailsResponse = UserDetails & {
+  message: string;
+};
+
+interface UserDetailsActions {
+  actions: {
+    getUserDetails: (
+      id: string,
+      token: string,
+      cookie: string,
+    ) => Promise<{ response: Response; user: UserDetailsResponse }>;
+    updateAvatar: (
+      imageURL: string,
+    ) => Promise<{ response: Response; result: UserDetailsResponse }>;
+    updateDisplayName: (
+      name: string,
+    ) => Promise<{ response: Response; result: UserDetailsResponse }>;
+  };
 }
 
-export type AoNoteJwtPayload = JwtPayload & { _id: string; role: string };
+export interface UserDetailsStore extends UserDetails, UserDetailsActions {}
 
-export type UserAuthAction =
-  | { type: "LOGIN"; payload: UserAuth }
-  | { type: "LOGOUT"; payload: UserAuth };
-
-export interface UserAuthContextAction {
-  state: UserAuth;
-  dispatch: Dispatch<UserAuthAction>;
-}
-
-export type UserAction =
-  | { type: "GET_USER"; payload: UserDetails }
-  | { type: "UPDATE_AVATAR"; payload: string }
-  | { type: "UPDATE_DISPLAY_NAME"; payload: string };
-
-export interface UserContextAction {
-  state: UserDetails;
-  dispatch: Dispatch<UserAction>;
-}
+export type AoNoteJwtPayload = JwtPayload & UserAuth;
