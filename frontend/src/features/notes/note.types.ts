a98fede3eeca9f5fa86d6ac847250558981ffb04
Jam <moonbamijamofficial15@gmail.com>
2026-05-25 15:00:21 +0800

    update note types

--- frontend/src/features/notes/note.types.ts
+++ frontend/src/features/notes/note.types.ts
@@ -1,21 +1,28 @@
-import type { Dispatch } from "react";
+import type { MongoDbDefaults } from "../../shared/types/mongodb.types";
 
-export interface Note {
-  _id?: string;
+export interface Note extends MongoDbDefaults {
   title: string;
   content: string;
-  createdAt?: Date | string;
-  updatedAt?: Date | string;
 }
 
-export type NoteAction =
-  | { type: "GET_NOTES"; payload: Note[] }
-  | { type: "GET_NOTE"; payload: Note }
-  | { type: "ADD_NOTE"; payload: Note }
-  | { type: "DELETE_NOTE"; payload: { _id: string } }
-  | { type: "UPDATE_NOTE"; payload: Note };
+export interface NoteResponse extends Response {
+  message?: string;
+}
 
-export interface NoteContextAction {
-  state: Note[];
-  dispatch: Dispatch<NoteAction>;
+interface NoteActions {
+  actions: {
+    getNotes: () => Promise<{
+      response: NoteResponse;
+      result: Note[];
+    }>;
+    getNote: (id: string) => Promise<{ response: NoteResponse; result: Note }>;
+    addNote: (note: Note) => Promise<void>;
+    updateNote: (note: Note) => Promise<void>;
+    deleteNote: (_id: string | undefined) => Promise<void>;
+  };
+}
+
+export interface NoteStore extends NoteActions {
+  note: Note;
+  notes: Note[];
 }
